de016271379c9e7f311d2220b9d23d695e0c4ad7
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2022-11-17 13:02:19 +0000

    commit function

--- parse_csv.py
+++ parse_csv.py
@@ -1,3 +1,7 @@
 my_file = open('shampoo_sales.csv', 'r')
-print(my_file.read())
+lines = my_file.readlines()
+values = []
+for l in lines[1:]:
+    values.append(float(l.split(',')[1]))
+print(sum(values))
 my_file.close()
